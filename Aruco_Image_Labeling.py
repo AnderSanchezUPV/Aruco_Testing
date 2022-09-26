e77fce7c12ec74c9b32d99b0a6d10c247c26bc64
AnderSanchezUPV <ander.sanchez@ehu.eus>
2022-09-26 10:39:17 +0200

    new filter algoritmh

--- Aruco_Image_Labeling.py
+++ Aruco_Image_Labeling.py
@@ -19,7 +19,7 @@ arucoParameters = cv2.aruco.DetectorParameters_create()
 aruco_marker_side_length=0.065 # Tamaño del lado del Aruco en m
 
 #   Definir carpeta de origen y Crear carpeta en la que colocar las nuevas imagenes
-Path_Origen = filedialog.askdirectory(initialdir = r'C:\Users\ander\Documents\Imagenes CoMAr') # Returns opened path as str
+Path_Origen = filedialog.askdirectory(initialdir = r'D:\CoMAr Data\BBDD\Bag Images') # Returns opened path as str
 #Path_Origen=r"C:\Users\ander\Documents\Imagenes CoMAr\2022_05_12_12_14_21"
 Path_Destino=Path_Origen+"_Etiquetas"
 
@@ -31,41 +31,52 @@ for filename in os.listdir(Path_Origen):
     corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(img, aruco_dict, 
                                         parameters=arucoParameters)
 
-    _centerY = int((corners[0][0][0][1] + corners[0][0][2][1]) / 2)
-    _centerX = int((corners[0][0][0][0] + corners[0][0][2][0]) / 2)                                    
-
-    rvecs, tvecs, obj_points = cv2.aruco.estimatePoseSingleMarkers(
-                                    corners,
-                                    aruco_marker_side_length,
-                                    mtx,
-                                    dist)
-
-    frame, axis_values =Ps.pose_stimation(img,ids,mtx,dist,rvecs,tvecs)
-
-    
     #   Generar Etiquetas
     name, ext =os.path.splitext(filename)
     label_name=name+'.txt'
-    f= open(os.path.join(Path_Destino,label_name),"w+")
+    f= open(os.path.join(Path_Destino,label_name),"a+")
 
-     #   Para guardar el primer aruco detectado
-    pos_x=tvecs[0][0][0]
-    pos_y=tvecs[0][0][1]
-    pos_z=tvecs[0][0][2]
+    for i in range(ids.shape[0]):
+        #if ids.shape[0]>1: 
+            #print('!!')
+        _centerY = int((corners[i][0][0][1] + corners[i][0][2][1]) / 2)
+        _centerX = int((corners[i][0][0][0] + corners[i][0][2][0]) / 2)
+        _cornerX= corners[i][0][0][0]                                 
+        _cornerY= corners[i][0][0][1]
+        rvecs, tvecs, obj_points = cv2.aruco.estimatePoseSingleMarkers(
+                                        corners,
+                                        aruco_marker_side_length,
+                                        mtx,
+                                        dist)
 
-    px_x=_centerX   #Pos del cel centro del Aruco
-    px_y=_centerY   #Pos del cel centro del Aruco
+        frame, axis_values =Ps.pose_stimation(img,ids,mtx,dist,rvecs,tvecs)
 
-    roll_x=axis_values[0][0] # Eje verde
-    pitch_y=axis_values[0][1]
-    yaw_z=axis_values[0][2]
+    
 
-    id_ar=ids[0][0]
-    f.write("{:.3f},{:.3f},{:.3f},{},{},{:.2f},{:.2f},{:.2f},{}".format( 
-            float(pos_x),float(pos_y),float(pos_z),
-            int(px_x),int(px_y),roll_x,pitch_y,yaw_z,
-            id_ar))
-      
+
+        #   Para guardar el primer aruco detectado
+        pos_x=tvecs[i][0][0]
+        pos_y=tvecs[i][0][1]
+        pos_z=tvecs[i][0][2]
+
+        px_x=_centerX   #Pos del cel centro del Aruco
+        px_y=_centerY   #Pos del cel centro del Aruco
+
+        px_Cx=_cornerX          #Pos de la esquina caracteristica del Aruco
+        px_Cy=_cornerY          #Pos de la esquina caracteristica del Aruco
+
+        roll_x=axis_values[i][0] # Eje verde
+        pitch_y=axis_values[i][1]
+        yaw_z=axis_values[i][2]
+
+        id_ar=ids[i][0]
+        f.write("{},{},{},{:.2f},{:.2f},{:.2f},{},{}\n".format( 
+                id_ar,
+                int(px_x),int(px_y),
+                roll_x,pitch_y,yaw_z,
+                int(px_Cx),int(px_Cy)                
+                ))
+    f.close()
 
     #   Para guardar todos los Arucos registrados en la imagen 
     # for i in range(ids.shape[0]):
--- Aruco_Image_filter.py
+++ Aruco_Image_filter.py
@@ -12,21 +12,26 @@ aruco_dict=cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_1000)
 arucoParameters = cv2.aruco.DetectorParameters_create()
 
 #   Definir carpeta de origen y Crear carpeta en la que colocar las nuevas imagenes
-Path_Origen=r"C:\Users\ander\OneDrive\Proyectos UPV-EHU\Proyecto Mercedes\Imagenes CoMAr\2022_01_20_12_40_16"
+Path_Origen=r"D:\CoMAr Data\BBDD\Bag Images\Slow_Ligth"
 New_Path=Path_Origen+"_Filtradas"
 
 #   Crear carpeta de destino si no existe
 if not os.path.exists(New_Path): os.mkdir(New_Path)
 
 images = []
-i=1
+
 for filename in os.listdir(Path_Origen):
     img = cv2.imread(os.path.join(Path_Origen,filename))
     corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(img, aruco_dict, 
                                         parameters=arucoParameters)
+    mainImgName, file_extension = os.path.splitext(filename)                                   
     if img is not None and corners!=[]:
+        #for i in range(ids.shape[0]):
         #images.append(img)
-        cv2.imwrite(os.path.join(New_Path,filename),img)
+
+            #Id_tag="_ID_{}".format(i)
+            #imgName=mainImgName+Id_tag+file_extension
+        cv2.imwrite(os.path.join(New_Path,mainImgName+file_extension),img)
     
 
 print('Done!')
